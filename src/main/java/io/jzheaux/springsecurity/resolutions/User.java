a75719d3e1ca75ac4d20572fd7c08f0d1772a59a
maysawyer1 <sawyer1@vt.edu>
2022-07-21 15:29:51 -0400

    Steps 1-5

--- src/main/java/io/jzheaux/springsecurity/resolutions/User.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/User.java
@@ -1,6 +1,30 @@
 package io.jzheaux.springsecurity.resolutions;
 
 import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Column;
+import javax.persistence.Id;
+import java.util.UUID;
 
+@Entity(name="users")
 public class User implements Serializable {
+    @Id
+    UUID id;
+
+    @Column
+    String username;
+
+    @Column
+    String password;
+
+    @Column
+    boolean enabled = true;
+
+    User() {}
+
+    public User(String username, String password) {
+        this.id = UUID.randomUUID();
+        this.username = username;
+        this.password = password;
+    }
 }
